72d892bfe36517b1b381f1ec569f8ab7992e89ec
DracKhan <fabio.garciap@udea.edu.co>
2024-03-29 20:53:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    //liberar memoria de la funcion CrearM, recordar implementarla de verdad
+    //for (unsigned int i = 0; i < n; ++i) {
+        //delete[] M[i];
+    //}
+    //delete[] M;
+
     cout << "Hello World!" << endl;
     return 0;
 }
