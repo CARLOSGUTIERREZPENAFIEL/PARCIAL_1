3a65dc17e51f9e443aaacc06f96dfc91f716e26c
DracKhan <fabio.garciap@udea.edu.co>
2024-03-29 22:04:06 -0500

    Correción
    Por error se eliminó el código implementado, se reescribe y se envía.

--- MODULO1.cpp
+++ MODULO1.cpp
@@ -2,3 +2,26 @@
 
 using namespace std;
 
+int** CrearM(unsigned int n) {
+    int** M = new int*[n];
+    for (unsigned int i = 0; i < n; ++i) {
+        M[i] = new int[n];
+    }
+
+    unsigned int valor = 1;
+    unsigned int inicio = 0;
+    unsigned int limiteM = n;
+    unsigned int CentroM = n / 2;
+
+    while (inicio < n) {
+        for (unsigned int j = 0; j < limiteM; ++j) {
+            if (inicio == CentroM && j == CentroM) {
+                M[inicio][j] = 0;
+            } else {
+                M[inicio][j] = valor++;
+            }
+        }
+        ++inicio;
+    }
+    return M;
+}
