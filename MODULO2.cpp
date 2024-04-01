814df1f98e2a79fe946f1e0a9bd2803b5790a260
CARLOSGUTIERREZPENAFIEL <164775504+CARLOSGUTIERREZPENAFIEL@users.noreply.github.com>
2024-04-01 10:03:27 -0500

    funciones
    se adjuntan nuevas funciones

--- MODULO2.cpp
+++ MODULO2.cpp
@@ -25,20 +25,21 @@ void rotarM(int **matriz, unsigned int orden){
     delete[] puntero_copia;
 
 }
-int* validar_Regla(){
-    int *regla, Ncerraduras,temporal;
-    bool confirmar = true;
-    cout<<"cuantas estructuras desea tener para su cerradura? "<<endl;
-    cin>>Ncerraduras;
+int* validar(){
+    int* regla, *temporal;
+    unsigned int capacidad, Nelementos;
+    int dato, dimension = 2;
+    char continuar = 's';
+    bool confirmar =  true;
     while(confirmar){
-        regla = new int[Ncerraduras+2];
-        for(int i = 0; i<2; i++){
+        regla = new int[2];
+        for(unsigned int i = 0; i<2; i++){
             if(i == 0){
-                cout<<"Ingresar el numero de fila: "<<endl;
+                cout<<"Ingresar un valor para K: "<<endl;
                 cin>>regla[i];
             }
             else{
-                cout<<"Ingrese el numero de columna: "<<endl;
+                cout<<"Ingrese un valor para K: "<<endl;
                 cin>>regla[i];
             }
         }
@@ -50,19 +51,86 @@ int* validar_Regla(){
             delete[] regla;
         }
     }
-    cout<<"A continuacion ingresar las condiciones para la comparacion (-1,0,1) :"<<endl;
-    for(int i = 2; i<Ncerraduras+1; i++){
-        cout<<"ingresar una condicion: "<<endl;
-        cin>>temporal;
-        while(temporal != -1 && temporal != 0 && temporal != 1){
-            cout<<"ingresar una condicion valida (-1,0,1)"<<endl;
-            cin>> temporal;
+    while(continuar == 's' || continuar == 'S'){
+        cout<<"ingresar un valor para K (-1,0,1): "<<endl;
+        cin>>dato;
+        if(dato == -1 || dato == 0 || dato == 1){
+            dimension += 1;
+            temporal = new int[dimension];
+            for(unsigned int i = 0; i < dimension-1; i++){
+                temporal[i] = regla[i];
+            }
+            temporal[dimension-1] = dato;
+            delete[] regla;
+            regla = new int[dimension];
+            for(unsigned int i = 0; i< dimension; i++ ){
+                regla[i]= temporal[i];
+            }
+            delete[] temporal;
+            cout<<"quiere ingresar otro valor para k? \n si es asi ingrese la letra 'S' sino ingrese otra letra : "<<endl;
+            cin>> continuar;
         }
-        regla[i]= temporal;
     }
-    for(int i = 0; i < Ncerraduras+1; i++){
-        cout<<regla[i]<<endl;
+    temporal = new int[dimension+1];
+    temporal[0] = dimension;
+    for(unsigned int i = 1; i<= dimension; i++){
+        temporal[i] = regla[i-1];
     }
-    return regla;
+
+    delete[] regla;
+    return temporal;
+
+}
+bool comparar_arreglos(int** matriz1,int** matriz2, unsigned int fila, unsigned int columna, unsigned int fila_temp, unsigned int columna_temp, int condicion){
+    bool confirmar;
+
+    //funcion que retornar true o false dependiendo de si dos arreglos cumplen una condicion
+    unsigned int numero1,numero2;
+    if(condicion == -1){
+        if(matriz1[fila][columna] < matriz2[fila_temp][columna_temp]){
+            cout<<"m1 "<<matriz1[fila][columna]<<endl<<"m2 "<<matriz2[fila_temp][columna_temp]<<endl;
+            confirmar = true;
+        }
+        else{
+            confirmar = false;
+        }
+    }
+    else if(condicion == 0){
+        if(matriz1[fila][columna] == matriz2[fila_temp][columna_temp]){
+            confirmar = true;
+        }
+        else{
+            confirmar = false;
+        }
+    }
+    else if(condicion == 1){
+        if(matriz1[fila][columna] > matriz2[fila_temp][columna_temp]){
+            cout<<"m1 "<<matriz1[fila][columna]<<endl<<"m2 "<<matriz2[fila_temp][columna_temp]<<endl;
+            confirmar = true;
+        }
+        else{
+            confirmar = false;
+        }
+    }
+
+    return confirmar;
+
+}
+void liberar_memoria(int** matriz, unsigned int dimension){
+    for(unsigned int i = 0; i < dimension; ++i) {
+        delete[] matriz[i];
+    }
+    delete[] matriz;
+}
+void cambiar_matriz(int** matriz1,int** matriz2, unsigned int dimension){
+    matriz1 = new int*[dimension];
+    for(unsigned int i = 0; i< dimension; i++){
+        matriz1[i] = new int[dimension];
+    }
+    for(unsigned int i = 0; i< dimension; i++){
+        for(unsigned int j = 0; j< dimension; j++){
+            matriz1[i][j] = matriz2[i][j];
+        }
+    }
 
 }
