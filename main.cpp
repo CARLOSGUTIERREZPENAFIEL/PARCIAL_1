0e7d3589dae3c69abe963d29f304d852f61a98f1
DracKhan <fabio.garciap@udea.edu.co>
2024-04-02 01:52:43 -0500

    Arreglo y orden en el main
    Se envia una actualización eliminando funciones, variables, y contenido innecesario, ademas de optimizar y arreglar errores que no dejaban que el main funcionara correctamente

--- main.cpp
+++ main.cpp
@@ -4,128 +4,112 @@ using namespace std;
 
 int main()
 {
-    unsigned int dimensionM1, dimension_temporal1,dimension_temporal2, fila, columna,fila_temp,columna_temp;
-    int *matriz,matriz_comparar1,*matriz_comparar2, *regla,*cerradura, Ncondicion = 0, longitud_regla, numero,condicion_abrir, rotar = 3, numero_cerradura = 1;
-    bool confirmar = false;
-    regla = validar();
-    longitud_regla = regla[0];
-    cerradura = new int[longitud_regla];
-    fila = regla[1] ;
-    fila_temp = regla[1] ;
-    columna_temp = regla[2];
-    columna = regla[2];
-    cout<<"longitud de regla: "<<longitud_regla<<endl;
-    cout<<"fila: "<<fila<<endl;
-    cout<<"colum: "<<columna<<endl;
 
-    if(fila > columna){
-        if(fila%2 !=0){
-            dimensionM1 = fila;
-            dimension_temporal1 = fila;
-            dimension_temporal2 = fila;
-        }
-        else {
-            dimensionM1 = fila + 1;
-            dimension_temporal1 = fila + 1;
-            dimension_temporal2 = fila + 1;
-        }
+    int pCondicion = 3;
+    int nMayor;
+    int* resultadoValidacion = validar();
+    int Tam_K = resultadoValidacion[0];
+    int Condiciones = Tam_K-2;
+    if (resultadoValidacion[1] > resultadoValidacion[2]){
+        nMayor = resultadoValidacion[1];
     }
-
-    else if(columna > fila){
-        if(columna%2 != 0){
-            dimensionM1 = columna;
-            dimension_temporal1 = columna;
-            dimension_temporal2 = columna;
-        }
-        else {
-            dimensionM1 = columna + 1;
-            dimension_temporal1 = columna + 1;
-            dimension_temporal2 = columna +1;
-        }
+    else{
+        nMayor = resultadoValidacion[2];
     }
-    matriz = CrearM(dimensionM1);
-    cerradura[0] = dimensionM1;
-    matriz_comparar1 = CrearM(dimension_temporal1);
-    matriz_comparar2 = CrearM(dimension_temporal2);
+    if ((nMayor%2)==0){
+        nMayor++;
+    }
+    int pCambiante1[2];
+    pCambiante1[0] = resultadoValidacion[1];
+    pCambiante1[1] = resultadoValidacion[2];
+    int pCambiante2[2];
+    pCambiante2[0] = pCambiante1[0];
+    pCambiante2[1] = pCambiante1[1];
+    int nCambiante = nMayor;
+    int** M_Base = CrearM(nMayor);
+    int dimension = nMayor;
+    int c_Matricez=resultadoValidacion[0]-1;
+    int X[c_Matricez];
+    int P[c_Matricez];
+    int pArreglos = 1;
+    int** Matriz_B = CrearM(nCambiante);
+    X[0]=nMayor;
+    P[0]=0;
 
-    for(unsigned int i = 3; i<longitud_regla; i++){
-        while(confirmar == false){
-            rotar = 3;
-            condicion_abrir = regla[i];
-            confirmar = comparar_arreglos(matriz_comparar1,matriz_comparar2, fila -1, columna -1, fila_temp -1, columna_temp-1 ,condicion_abrir);
-            if(confirmar == false){
-                while(rotar >0){
-                    rotarM(matriz_comparar2, dimension_temporal2);
-                    confirmar = comparar_arreglos(matriz_comparar1,matriz_comparar2, fila-1, columna-1, fila_temp-1, columna_temp-1, condicion_abrir);
-                    if(confirmar == true){
-                        cout<<"hola"<<endl;
-                        rotar = -3;
-                        cerradura[numero_cerradura] = dimension_temporal2;
-                        liberar_memoria(matriz_comparar1,dimension_temporal1);
-                        cambiar_matriz(matriz_comparar1,matriz_comparar2,dimension_temporal2);
-                        liberar_memoria(matriz_comparar2,dimension_temporal2);
-                        dimension_temporal1 = dimension_temporal2;
-                        dimension_temporal2 = dimensionM1;
-                        matriz_comparar2 = CrearM(dimension_temporal2);
-                        numero_cerradura += 1;
+    while(Condiciones >= 1){
+        bool Comparar = false;
+        while(Comparar == false){
+            int rotacion=0;
+
+            while(rotacion<=3){
+
+                Comparar =  comparar_arreglos(M_Base, Matriz_B, pCambiante1[0]-1, pCambiante1[1]-1, pCambiante2[0]-1, pCambiante2[1]-1, resultadoValidacion[pCondicion]);
+
+                if(Comparar == false){
+                    rotarM(Matriz_B, nCambiante);
+                    rotacion++;
+
+                }
+                else{
+                    X[pArreglos]=nCambiante;
+                    P[pArreglos]=rotacion;
+                    pArreglos++;
+                    liberar_memoria(M_Base,dimension);
+                    cambiar_matriz(M_Base,Matriz_B, nCambiante);
+                    dimension = nCambiante;
+
+                    liberar_memoria(Matriz_B,nCambiante);
+                    nCambiante = nMayor;
+                    Matriz_B = CrearM(nCambiante);
+                    pCambiante1[0] = pCambiante2[0];
+                    pCambiante1[1] = pCambiante2[1];
+                    pCambiante2[0] = resultadoValidacion[1];
+                    pCambiante2[1] = resultadoValidacion[2];
+
+                    rotacion = 10;
+                }
+
+                if(rotacion==4){
+                    liberar_memoria(Matriz_B, nCambiante);
+                    nCambiante+=2;
+                    Matriz_B = CrearM(nCambiante);
+
+                    if(dimension < nCambiante){
+                        pCambiante1[0] = pCambiante2[0];
+                        pCambiante1[1] = pCambiante2[1];
+                        pCambiante2[0]++;
+                        pCambiante2[1]++;
+
                     }
                     else{
-                        cout<<"hola4"<<endl;
-                        rotar = rotar - 1;
-                        cout<<"rotarrrrrrr "<<rotar<<endl;
+                        pCambiante2[0]++;
+                        pCambiante2[1]++;
                     }
-
                 }
-                if(rotar  == 0){
-                    cout<<"hola3"<<endl;
-                    liberar_memoria(matriz_comparar2,dimension_temporal2);
-                    fila = fila_temp;
-                    columna = columna_temp;
-                    dimension_temporal2 += 2;
-                    matriz_comparar2 = CrearM(dimension_temporal2);
-                    fila_temp += 1;
-                    columna_temp += 1;
-                }
-
-
             }
-            else{
-                cout<<"hola2"<<endl;
-                cerradura[numero_cerradura] =  dimension_temporal2;
-                liberar_memoria(matriz_comparar1,dimension_temporal1);
-                cambiar_matriz(matriz_comparar1,matriz_comparar2,dimension_temporal2);
-                liberar_memoria(matriz_comparar2,dimension_temporal2);
-                dimension_temporal1 = dimension_temporal2;
-                dimension_temporal2 = dimensionM1;
-                matriz_comparar2 = CrearM(dimension_temporal2);
-                fila = fila_temp;
-                columna = columna_temp;
-                fila_temp = regla[1];
-                columna_temp = regla[2];
-                numero_cerradura += 1;
-                cout<<"pruebaaaaaaa"<<endl;
-            }
-
-
-
         }
-        confirmar = false;
+
+        Condiciones--;
+        pCondicion++;
+
     }
-
-    liberar_memoria(matriz,dimensionM1);
-    liberar_memoria(matriz_comparar1,dimension_temporal1);
-    liberar_memoria(matriz_comparar2,dimension_temporal2);
-    for(unsigned int i = 0; i<longitud_regla -1; i++){
-        cout<<cerradura[i]<<",";
+    cout << "Para una regla K: (";
+    for(int i = 1; i < Tam_K; i++) {
+        cout << resultadoValidacion[i] << ", ";
     }
-    delete[] cerradura;
+    cout << resultadoValidacion[Tam_K] << ") una posible cerradura podria ser:" << endl << "X(";
+    for(int i = 0; i < c_Matricez-1; i++) {
+        cout << X[i] << ", ";
+    }
+    cout << X[c_Matricez-1] << ")" << endl << "Esta cerradura tendria las siguientes rotaciones en su respectivo orden a X:" << endl <<"R(";;
 
-    delete[] regla;
-
-
-
-
+    for(int i = 0; i < c_Matricez-1; i++) {
+        cout << P[i] << ", ";
+    }
+    cout << P[c_Matricez-1] << ")" << endl;
+    delete[] resultadoValidacion;
+    liberar_memoria(M_Base, dimension);
+    liberar_memoria(Matriz_B, nCambiante);
 
     return 0;
-
 }
